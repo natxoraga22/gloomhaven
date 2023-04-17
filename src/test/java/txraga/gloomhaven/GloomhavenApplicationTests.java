c46d47198fb5e1d2c5e9d58ddafd1e37512b342c
Natxo Raga <natxoraga@me.com>
2023-04-18 01:34:28 +0200

    Initial project

---
+++ src/test/java/txraga/gloomhaven/GloomhavenApplicationTests.java
@@ -0,0 +1,13 @@
+package txraga.gloomhaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GloomhavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
